b3a20a4886d621925bbedccbdd40bc2fa414c362
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2025-05-10 17:06:54 -0600

    Initial Setup

---
+++ metadata-svc/src/main/java/com/example/datalake/metadatasvc/MetadataSvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.datalake.metadatasvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetadataSvcApplication.class, args);
+	}
+
+}
---
+++ processing-svc/src/main/java/com/example/datalake/processingsvc/ProcessingSvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.datalake.processingsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingSvcApplication.class, args);
+	}
+
+}
